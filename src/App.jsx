d256880b485641c4fa03234b808fb37ca1cec5e6
Prathvin Pinto <prathvinpinto01@gmail.com>
2026-04-17 02:15:02 +0530

    Added update check button (POC)

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,34 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import { check } from "@tauri-apps/plugin-updater";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [status, setStatus] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
+  async function checkUpdate() {
+    setStatus("Checking...");
+
+    try {
+      const update = await check();
+
+      if (update?.available) {
+        setStatus(`Update available: ${update.version}`);
+      } else {
+        setStatus("No updates available");
+      }
+    } catch (err) {
+      console.error("Updater error:", err);
+      setStatus(String(err));
+    }
   }
 
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+    <div style={{ padding: 30 }}>
+      <h1>StashyJD</h1>
 
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      <button onClick={checkUpdate}>Check for Updates</button>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+      <p>{status}</p>
+    </div>
   );
 }
 
